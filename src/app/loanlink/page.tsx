1a25a15289b7a99496a0a3445b22eda12c764cdc
Ralph Uy <areaoear@gmail.com>
2025-05-04 04:32:03 -1000

    issue-25 styling

--- src/app/loanlink/page.tsx
+++ src/app/loanlink/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+
 'use client';
 
 import { useChat } from 'ai/react';
@@ -9,40 +11,40 @@ export default function LoanLink() {
     initialMessages: [
       {
         id: crypto.randomUUID(),
-        role: 'user',
-        content: 'Hello! Ask me about resources that you might need!',
+        role: 'Loanlink',
+        content: 'Hello! Ask me about resources that you might want to borrow!',
       },
     ],
   });
 
   return (
-    <Container id="hasBG" style={{ display: 'flex', flexDirection: 'row', height: '100vh', overflow: 'hidden' }}>
-      <div className="mx-auto max-w-md py-24 flex flex-col stretch items-center" style={{ width: '100vw' }}>
-        <div className="max-w-2xl p-4 bg-white rounded-lg shadow-md">
-          <div className="flex flex-col space-y-4">
-            {messages.length > 0
-              ? messages.map((message) => (
-                <div
-                  key={message.id}
-                  className={`p-2 rounded-lg ${
-                    message.role === 'user' ? 'bg-blue-100' : 'bg-gray-100'
-                  }`}
-                >
-                  {message.content}
-                </div>
-              ))
-              : null }
-          </div>
-          <form onSubmit={handleSubmit} className="mt-4">
-            <input
-              type="text"
-              value={input}
-              onChange={handleInputChange}
-              placeholder="Type your message..."
-              className="fixed bottom-0 w-full p-2 border border-gray-300 rounded-lg items-center"
-            />
-          </form>
+
+    <Container id="hasBG" style={{ display: 'flex', height: '100vh', overflow: 'hidden', justifySelf: 'center', alignSelf: 'center', backgroundSize: 'cover', minWidth: '100%', minHeight: '100vh' }}>
+      <div className="p-4 mx-auto my-auto" style={{ width: '80%', height: '90%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', backgroundColor: '#095A3E' }}>
+        <div className="flex flex-col" style={{ height: '90%', overflowY: 'auto', backgroundColor: '#363636', padding: '20px' }}>
+          {messages.length > 0
+            ? messages.map((message) => (
+              <div
+                key={message.id}
+                className={`p-2 rounded-lg ${
+                  message.role === 'User' ? 'bg-blue-100' : 'bg-gray-100'
+                }`}
+                style={{ color: 'white', fontSize: '1.2rem', fontWeight: '550' }}
+              >
+                {`${message.role}: ${message.content}`}
+              </div>
+            ))
+            : null }
         </div>
+        <form onSubmit={handleSubmit} className="">
+          <input
+            type="text"
+            value={input}
+            onChange={handleInputChange}
+            placeholder="Type your message..."
+            className="fixed bottom-0 w-100 p-2 border"
+          />
+        </form>
       </div>
     </Container>
   );
