4e57e3e6132e065ac6624155f321c719776c2acd
Ralph Uy <areaoear@gmail.com>
2025-05-04 04:49:43 -1000

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -30,7 +30,7 @@ const AdminPage = async () => {
           <Col>
             <h1>Resources</h1>
             {resources.map((res) => (
-              <div key={res.id} className="border rounded p-3 mb-3" style={{ width: '50px' }}>
+              <div key={res.id} className="border rounded p-3 mb-3" style={{ width: '500px' }}>
                 <h4>{res.name}</h4>
                 <p>
                   <strong>Owner:</strong>
