0a34672686f287990d1c5f0b18b4dbe5a70fce65
Ralph Uy <areaoear@gmail.com>
2025-05-04 04:55:07 -1000

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -26,9 +26,9 @@ const AdminPage = async () => {
       </div>
 
       <Container fluid className="py-3">
+        <h1>Resources</h1>
         <Row>
-          <Col className="d-flex flex-row align-items-center">
-            <h1>Resources</h1>
+          <Col className="d-flex flex-row align-items-center mx-2">
             {resources.map((res) => (
               <div key={res.id} className="border rounded p-3 mb-3" style={{ width: '250px' }}>
                 <h4>{res.name}</h4>
