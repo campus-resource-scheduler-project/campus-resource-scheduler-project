cf1f2fe63f3af6f35072df45ab742983d9dc7dad
Ralph Uy <areaoear@gmail.com>
2025-05-07 22:44:58 -1000

    flattening array

--- src/app/api/loanlink/route.ts
+++ src/app/api/loanlink/route.ts
@@ -19,7 +19,7 @@ export async function POST(req: Request) {
   const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
     stream: true,
-    messages: [customInstructions, messages.map((message: any) => ({
+    messages: [customInstructions, ...messages.map((message: any) => ({
       role: message.role,
       content: message.content,
     })),
